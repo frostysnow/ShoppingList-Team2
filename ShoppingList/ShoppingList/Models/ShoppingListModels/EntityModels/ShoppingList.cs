89cc642bc3e9afa947fdf34996ecb70fbd9d6f65
Scott Kelly <sctkelly@gmail.com>
2016-05-24 14:22:02 -0400

    Added service layer, Identity model, and added priority enum to shoppinglistmodel.

--- ShoppingList/ShoppingList/Models/ShoppingListModels/EntityModels/ShoppingList.cs
+++ ShoppingList/ShoppingList/Models/ShoppingListModels/EntityModels/ShoppingList.cs
@@ -15,6 +15,8 @@ namespace ShoppingList.Models.ShoppingListModels
         [Required]
         public int ListId                       { get; set; }
         [Required]
+        public Guid OwnerId                     { get; set; }
+        [Required]
         [ForeignKey("UserId")]
         public int UserId                       { get; set; }
         public string Color                     { get; set; }
